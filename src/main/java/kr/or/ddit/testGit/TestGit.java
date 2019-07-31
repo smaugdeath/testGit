f8096a6df2c403ba4d773bdb68eb2a741f5df8cc
jychoi <smaugdeath@gmail.com>
2019-07-31 16:55:33 +0900

    first commit testGit

---
+++ src/main/java/kr/or/ddit/testGit/TestGit.java
@@ -0,0 +1,36 @@
+package kr.or.ddit.testGit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestGit extends HttpServlet{
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		resp.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter pw = resp.getWriter();
+		
+		pw.write("<html>");
+		pw.write("<body>");
+		pw.write("Hello World Servlet : " + sdf.format(date));
+		pw.write("</body>");
+		pw.write("</html>");
+		
+	}
+	
+	
+
+}
